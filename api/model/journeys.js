318c7c1c63b087f9ba8f48c4bdcf1d0011275749
Gary Daw <garydaw@ntlworld.com>
2024-11-03 17:45:34 +0000

    fix to query

--- api/model/journeys.js
+++ api/model/journeys.js
@@ -9,7 +9,7 @@ journeys.get = async () => {
     sql += "LEFT OUTER JOIN journey_guide jg ";
     sql += " ON jg.base_id = u.base_id ";
     sql += "WHERE u.is_galactic_legend = 1 "
-    sql += "AND jg.is_current = 1 "
+    sql += "AND IFNULL(jg.is_current,1) = 1 "
     sql += "ORDER BY u.character_name";
 
     const guides = await runSQL(sql, []);
