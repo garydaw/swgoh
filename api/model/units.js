78815cbdc14d639e83782658bc64af018c340396
Gary Daw <garydaw@ntlworld.com>
2025-07-30 13:07:59 +0100

    Start of best mods

--- api/model/units.js
+++ api/model/units.js
@@ -35,7 +35,7 @@ units.get = async (ally_code, base_id, combat_type) => {
 units.getGeneric = async (combat_type) => {
 
     let sql = "";
-    sql += "SELECT u.base_id, u.character_name, u.alignment, u.role, ";
+    sql += "SELECT u.base_id, u.character_name, u.alignment, u.role, u.url, ";
     sql += "        u.categories, u.unit_image, ";
     sql += "        13 AS gear_level, 0 AS gear_level_plus, ";
     sql += "        85 AS level, 0 AS power, 7 AS rarity, 0 AS zeta_abilities, 0 AS omicron_abilities, ";
@@ -110,6 +110,111 @@ units.refreshUnits = async () => {
 
     return;
 
-} 
+}
+
+units.setBestMods = async (base_id, url, character_name) => {
+
+    await runSQL("DELETE FROM unit_mod WHERE base_id = ?", [base_id]);
+
+    const response = await axios.get('https:' + url + 'best-mods/');
+
+    const html = response.data;
+    var mod_start = 0;
+
+    
+    //arrow
+    mod_text = "Best Arrow (Receiver)";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_text = "Mod Primary Stat: <strong>";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+
+    //no mods found
+    if(mod_start === mod_text.length - 1){
+        return;
+    }
+
+    mod_end = html.indexOf("</strong>", mod_start);
+    var arrow = html.substr(mod_start, mod_end - mod_start);
+
+    //triangle
+    var mod_text = "Best Triangle (Holo-Array)";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_text = "Mod Primary Stat: <strong>";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    var mod_end = html.indexOf("</strong>", mod_start);
+    var triangle = html.substr(mod_start, mod_end - mod_start);
+
+    //circle
+    mod_text = "Best Circle (Data-Bus)";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_text = "Mod Primary Stat: <strong>";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_end = html.indexOf("</strong>", mod_start);
+    var circle = html.substr(mod_start, mod_end - mod_start);
+
+    //Cross
+    mod_text = "Best Cross (Multiplexer)";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_text = "Mod Primary Stat: <strong>";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_end = html.indexOf("</strong>", mod_start);
+    var cross = html.substr(mod_start, mod_end - mod_start);
+
+    //sets
+    //mod_text = "The most popular Mod Set for " + htmlEncode(character_name) + " is";
+    mod_text = "The most popular Mod Set for " + character_name + " is";
+    mod_start = html.indexOf(mod_text, mod_start) + mod_text.length;
+    mod_end = html.indexOf("</p>", mod_start);
+    var full_set = html.substr(mod_start, mod_end - mod_start);
+
+    var set_start = 0;
+    var set = [];
+
+    var set_text = '<span class="fw-bold">';
+    set_start = full_set.indexOf(set_text, set_start) + set_text.length;
+    var set_end = full_set.indexOf("</span>", set_start);
+    set.push(full_set.substr(set_start, set_end - set_start));
+
+    //double set
+    if(full_set.substr(set_end + 9,1) === "4"){
+        set.push(full_set.substr(set_start, set_end - set_start));
+    }
+    
+    set_start = full_set.indexOf(set_text, set_start) + set_text.length;
+    var set_end = full_set.indexOf("</span>", set_start);
+    set.push(full_set.substr(set_start, set_end - set_start));
+
+    //double set
+    if(full_set.substr(set_end + 9,1) === "4"){
+        set.push(full_set.substr(set_start, set_end - set_start));
+    }
+
+    if(set.length === 2){
+        set_start = full_set.indexOf(set_text, set_start) + set_text.length;
+        var set_end = full_set.indexOf("</span>", set_start);
+        set.push(full_set.substr(set_start, set_end - set_start));
+    }
+    
+    let sql = "INSERT INTO unit_mod (base_id, slot_id, group_set_id, primary_stat) SELECT ?, ?, group_set_id, ? FROM group_set WHERE group_set_name = ?";
+
+    //square
+    await runSQL(sql, [base_id, 2, "Offense", set[0]]);
+
+    //arrow
+    await runSQL(sql, [base_id, 3, arrow, set[0]]);
+
+    //diamond
+    await runSQL(sql, [base_id, 4, "Defense", set[1]]);
+
+    //triangle
+    await runSQL(sql, [base_id, 5, triangle, set[1]]);
+
+    //circle
+    await runSQL(sql, [base_id, 6, circle, set[2]]);
+
+    //cross
+    await runSQL(sql, [base_id, 7, cross, set[2]]);
+
+}
 
 export default units;
--- api/routes/swgoh.js
+++ api/routes/swgoh.js
@@ -10,10 +10,19 @@ swgohRouter.get('/guild/:guild_id', async (req, res) => {
     const guild_id = req.params.guild_id;
 
     //get units
-    await units.refreshUnits();
+    //await units.refreshUnits();
 
     //get allies
-    await players.refreshAllies(guild_id);
+    //await players.refreshAllies(guild_id);
+
+    // get best unit mods
+    var all_units = await units.getGeneric(1);
+    
+    for(var u = 0; all_units.length > u; u++){
+        console.log("Setting best mods for " + all_units[u].character_name);
+        await units.setBestMods(all_units[u].base_id, all_units[u].url, all_units[u].character_name);
+        
+    }
 
 
     res.json({ message:"Guild refreshed"});
